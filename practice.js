4b7db325025716f34bdc07eb5887cfd51eef13fe
calimendoza <mbaivictor1@gmail.com>
2022-07-20 22:34:17 -0700

    added fuctions and pyramid of doom aka callback hell

--- practice.js
+++ practice.js
@@ -8,3 +8,42 @@ let stocks = {
 }
 
 //CREATE FUNCTIONS
+
+let order = (fruit_name, call_production) => {
+   
+    setTimeout(function() {
+        console.log(`${stocks.Fruits[fruits_name]} was selected`)
+
+        call_production()
+        
+        }, 2000)
+    
+}
+
+let production = () =>{
+
+    setTimeout(()=>{
+      console.log("production has started")
+      setTimeout(()=>{
+        console.log("The fruit has been chopped")
+        setTimeout(()=>{
+          console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`)
+          setTimeout(()=>{
+            console.log("start the machine")
+            setTimeout(()=>{
+              console.log(`Ice cream placed on ${stocks.holder[1]}`)
+              setTimeout(()=>{
+                console.log(`${stocks.toppings[0]} as toppings`)
+                setTimeout(()=>{
+                  console.log("serve Ice cream")
+                },2000)
+              },3000)
+            },2000)
+          },1000)
+        },1000)
+      },2000)
+    },0000)
+  
+  };
+
+order(0, production)
