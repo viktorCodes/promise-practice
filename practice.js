41cc747047792838845a84c5acaf80c99b5f706d
calimendoza <mbaivictor1@gmail.com>
2022-07-21 00:00:44 -0700

    refactored code to async/await

--- practice.js
+++ practice.js
@@ -11,74 +11,50 @@ let stocks = {
 
 let is_shop_open = true;
 
-let order = ( time, work ) => {
+function time(ms) {
 
-    return new Promise( ( resolve, reject )=>{
-  
-      if( is_shop_open ){
-  
-        setTimeout(()=>{
-  
-         // work is 👇 getting done here
-          resolve( work() )
-  
-  // Setting 👇 time here for 1 work
-         }, time)
-  
-      }
-  
-      else{
-        reject( console.log("Our shop is closed") )
-      }
-  
-    })
-  }
-// Set 👇 time here
-order( 2000, ()=>console.log(`${stocks.Fruits[0]} was selected`))
-//    pass a ☝️ function here to start working
+    return new Promise( (resolve, reject) => {
+ 
+       if(is_shop_open){
+          setTimeout(resolve,ms);
+       }
+ 
+       else{
+          reject(console.log("Shop is closed"))
+       }
+     });
+ }
 
-// step 1
-order(2000,()=>console.log(`${stocks.Fruits[0]} was selected`))
+ async function kitchen(){
+    try{
+	await time(2000)
+	console.log(`${stocks.Fruits[0]} was selected`)
 
-// step 2
-.then(()=>{
-  return order(0000,()=>console.log('production has started'))
-})
+	await time(0000)
+	console.log("production has started")
 
-// step 3
-.then(()=>{
-  return order(2000, ()=>console.log("Fruit has been chopped"))
-})
+	await time(2000)
+	console.log("fruit has been chopped")
 
-// step 4
-.then(()=>{
-  return order(1000, ()=>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`))
-})
+	await time(1000)
+	console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
 
-// step 5
-.then(()=>{
-  return order(1000, ()=>console.log("start the machine"))
-})
+	await time(1000)
+	console.log("start the machine")
 
-// step 6
-.then(()=>{
-  return order(2000, ()=>console.log(`ice cream placed on ${stocks.holder[1]}`))
-})
+	await time(2000)
+	console.log(`ice cream placed on ${stocks.holder[1]}`)
 
-// step 7
-.then(()=>{
-  return order(3000, ()=>console.log(`${stocks.toppings[0]} as toppings`))
-})
+	await time(3000)
+	console.log(`${stocks.toppings[0]} as toppings`)
 
-// Step 8
-.then(()=>{
-  return order(2000, ()=>console.log("Serve Ice Cream"))
-})
+	await time(2000)
+	console.log("Serve Ice Cream")
+    }
 
-.catch(()=>{
-    console.log("Customer left")
-  })
-
-  .finally(()=>{
-    console.log("end of day")
-  })
+    catch(error){
+	 console.log("customer left")
+    }
+}
+ // Trigger
+ kitchen();
